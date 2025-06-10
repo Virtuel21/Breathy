53f7560fabc2a294dd64a9b0738bf838174fc57f
Virtuel <virtuel210@outlook.fr>
2025-06-10 19:14:35 +0200

    Initial Commit

---
+++ Breathy/BreathyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BreathyApp.swift
+//  Breathy
+//
+//  Created by Heux Julien on 10/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BreathyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BreathyTests/BreathyTests.swift
@@ -0,0 +1,16 @@
+//
+//  BreathyTests.swift
+//  BreathyTests
+//
+//  Created by Heux Julien on 10/06/2025.
+//
+
+import Testing
+
+struct BreathyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
